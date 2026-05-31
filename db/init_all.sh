37a509eb0f44b5156955f3720dae368b45673fa9
Junjey123-mx <verneljosue@gmail.com>
2026-05-31 14:13:07 -0600

    fix(db): include stock reservation script in project 3 initialization

--- db/init_all.sh
+++ db/init_all.sh
@@ -13,3 +13,4 @@ psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/project3/0
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/project3/05_views_project3.sql
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/project3/06_indexes_project3.sql
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/project3/07_permissions_project3.sql
+psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /sql/project3/09_stock_reservado.sql
